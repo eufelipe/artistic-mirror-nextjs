1ac893f4f1167e7ed0240b685662d5a596b87afc
Felipe Rosas <felipe.rosas@g.globo>
2023-03-25 14:01:35 -0300

    chore: add inicial home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,65 @@
+"use client";
+
+import "@tensorflow/tfjs-backend-webgl";
+import { useCallback, useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+import HandGestureDetector from "./components/HandGestureDetector";
+
 export default function Home() {
-  return <h1>Espelho Artistico</h1>;
+  const [emoji, setEmoji] = useState("");
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const handleGestureDetected = useCallback(
+    (gestureName: string) => {
+      setEmoji(gestureName);
+      console.log(emoji);
+    },
+    [setEmoji, emoji]
+  );
+
+  const width = globalThis.screen.availWidth;
+  const height = globalThis.screen.availHeight;
+
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: width,
+          height: height,
+          visibility: "hidden",
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: width,
+          height: height,
+        }}
+      />
+
+      <HandGestureDetector
+        onGestureDetected={handleGestureDetected}
+        webcamRef={webcamRef}
+        canvasRef={canvasRef}
+      />
+    </div>
+  );
 }
