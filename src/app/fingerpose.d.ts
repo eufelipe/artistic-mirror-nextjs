55094eb6a9ebe00657fa85f790abbbb19c97c57d
Felipe Rosas <felipe.rosas@g.globo>
2023-03-25 14:01:22 -0300

    chore: add types for fingerpose

---
+++ src/app/fingerpose.d.ts
@@ -0,0 +1,9 @@
+declare module "fingerpose" {
+  export const FingerPose: any;
+  export const Finger: any;
+  export const Pipeline: any;
+  export const GestureDescription: any;
+  export const GestureEstimator: any;
+  export const Gestures: any;
+  export const FingerCurl: any;
+}
