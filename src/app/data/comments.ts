20497d23fd4777d1ac73629cdc8376b3b9416fed
Felipe Rosas <felipe.rosas@g.globo>
2023-04-01 15:18:56 -0300

    chore: add datas

---
+++ src/app/data/comments.ts
@@ -0,0 +1,12 @@
+export default [
+  "Não posso deixar as falas ensaiadas soarem robóticas. Preciso buscar a naturalidade em cada palavra dita.",
+  "O figurino pode ser uma ferramenta importante para me ajudar a entrar na pele do personagem. Preciso aproveitar bem cada elemento do meu visual.",
+  "A conexão com os outros atores em cena é fundamental para transmitir veracidade e emoção para o público. É preciso estar atento e sensível aos movimentos e falas de todos.",
+  "A preparação física é essencial para aguentar o ritmo das apresentações. Preciso manter uma rotina de exercícios e cuidar da alimentação.",
+  "A preparação psicológica também é importante. É preciso lidar com o nervosismo e a pressão de forma saudável para poder dar o meu melhor no palco.",
+  "Cada ensaio é uma oportunidade para descobrir novas nuances do personagem e da cena. É preciso estar aberto para experimentar e explorar diferentes possibilidades.",
+  "A técnica é importante, mas não pode ser um fim em si mesma. É preciso usar a técnica a favor da emoção e da história que estamos contando.",
+  "O público é uma parte importante do processo. Precisamos estar atentos à sua reação e adaptar a performance de acordo com o feedback que recebemos.",
+  "A improvisação pode ser uma ferramenta valiosa em cena, mas é preciso saber usá-la com responsabilidade e sem comprometer a história que estamos contando.",
+  "A paixão pelo teatro é o que nos mantém firmes em meio aos desafios e dificuldades. Preciso sempre lembrar do amor que sinto por essa arte e do privilégio que é poder fazer parte dela.",
+];
